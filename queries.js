3519fb99978f690498f0851fcc2a7acd98758750
SamZRetzlaff <73126723+SamZRetzlaff@users.noreply.github.com>
2020-11-13 12:42:48 -0800

    added post a new user

--- queries.js
+++ queries.js
@@ -15,3 +15,23 @@ const getStudents = (req, res) => {
         res.status(200).json(results.rows)
     })
 }
+
+const getStudentById = (req, res) => {
+    const id = parseInt(req.params.id)
+    pool.query('SELECT * FROM students WHERE id = $1', [id], (error, results) => {
+        if(error) {
+            throw error
+        }
+        res.status(200).json(results.rows)
+    })
+}
+
+const createStudent = (req, res) => {
+    const {name, grades} = req.body
+    pool.query('INSERT INTO students (name, grades) VALUES ($1, $2)', [name, grades], (error, results) => {
+        if(error) {
+            throw error
+        }
+        res.status(201).send(`Student added with ID: ${result.insertId}`)
+    })
+}
