dc6b938718bf1235d86cd3c8c65bec5960f7592a
SamZRetzlaff <73126723+SamZRetzlaff@users.noreply.github.com>
2020-11-13 12:33:45 -0800

    ST set up GET all students

--- queries.js
+++ queries.js
@@ -6,3 +6,12 @@ const pool = new Pool({
     password: 'password',
     port: 5432,
 })
+
+const getStudents = (req, res) => {
+    pool.query('SELECT * FROM students ORDER BY id ASC', (error, results) => {
+        if(error) {
+            throw error
+        }
+        res.status(200).json(results.rows)
+    })
+}
